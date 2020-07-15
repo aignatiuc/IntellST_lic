b2ebe14791eba517a64eb4febf32555f83e5e7ee
aignatiuc <arina.ignatiuc@ee.utm.md>
2020-07-15 20:25:01 +0300

    Edited not found page

--- intellst-front/src/views/NotFound.vue
+++ intellst-front/src/views/NotFound.vue
@@ -1,7 +1,28 @@
 <template>
-  <v-container fluid>
-    <v-row justify="space-around">
-      <v-img :src="require('../assets/notfound.png')" aspect-ratio="1.9" />
-    </v-row>
-  </v-container>
+  <div id="notfound">
+    <v-card class="notfound-bg"></v-card>
+    <v-flex xs12 class="notfound">
+      <div class="notfound-404">
+        <h1 style="text-transform: uppercase;">404</h1>
+      </div>
+      <h2>
+        The page you are trying to get to never existed in this reality, or has
+        migrated to a parallel universe.
+      </h2>
+      <h2>
+        Try going back to home page and repeating your action. Or, contact
+        helpdesk for support.
+      </h2>
+      <v-btn
+        to="/"
+        text
+        color="error"
+        class="my-4"
+        rounded
+        large
+        style="background-color: white; font-weight: bold;"
+        >Go Home</v-btn
+      >
+    </v-flex>
+  </div>
 </template>
